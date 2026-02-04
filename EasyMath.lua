cdd63c02d432913418c6da31ae0e1d0c4410b6d5
Veiyn <vein.inquiry@gmail.com>
2026-02-04 12:31:22 -0600

    Create EasyMath.lua

---
+++ EasyMath.lua
@@ -0,0 +1,19 @@
+--!strict
+--@author: v_eiyn/ve1yn/veiyn
+
+
+local EasyMath = {}
+
+for _, child in ipairs(script.Modules:GetChildren()) do
+	if child:IsA("ModuleScript") then
+		local ok, module = pcall(require, child)
+
+		if ok then
+			EasyMath[child.Name] = module
+		else
+			warn("[EasyMath] Failed to load module:", child.Name, module)
+		end
+	end
+end
+
+return EasyMath
